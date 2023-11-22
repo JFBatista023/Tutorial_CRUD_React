d0be56921cc5df889ecc2b70f55d408ab900448e
MaskQuanly <debian.programmer.francisco@gmail.com>
2023-11-22 12:21:14 -0300

    correção de bugs e funcionalidade listar produtos

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,67 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { Button, TextField, Container, Grid, Paper, Typography, ThemeProvider, CssBaseline, FormLabel } from '@mui/material';
+import { Button, TextField, Container, Grid, Paper, Typography, ThemeProvider, CssBaseline, FormLabel, List, ListItem, ListItemText } from '@mui/material';
 import theme from './components/theme';
+import ProdutoDaApi from './components/modulos/ProdutoDaApi';
+import ProdutoParaApi from './components/modulos/ProdutoParaApi';
 
 const App = () => {
   const URL = 'http://127.0.0.1:8000';
 
-  const [produto, setProduto] = useState<{nome : string, imagem : File | null, preco : number}>({
-    nome: '',
-    preco: 0,
-    imagem: null,
+  const [dadosMovoProduto, setDadosNovoProduto] = useState<ProdutoParaApi>(new ProdutoParaApi(-1,'',0,null));
+  const [produtoEncontrado, setProdutoEncontrado] = useState<ProdutoDaApi>(new ProdutoDaApi(-1,'',0,null));
+  const [novoDadoProduto, setNovoDadoProduto] = useState<ProdutoParaApi>(new ProdutoParaApi(-1,'',0,null));
+  const [listaProdutos, setListaProdutos] = useState<ProdutoDaApi[]>([]);
+
+  const [idDeletarProduto, setIdDeletarProduto] = useState(-1);
+  const [idBuscarProduto, setIdBuscarProduto] = useState(-1);
+  
+  const [resultadosVisiveis, setResultadosVisiveis] = useState<{buscaItem : boolean, buscaItens : boolean}>({
+    buscaItem : false,
+    buscaItens : false
   });
 
-  const [produtoResgatado, setProdutoResgatado] = useState<{nome : string, imagem : string, preco : string}>({
-    nome: '',
-    preco: '',
-    imagem: '',
-  });
+  const handleChangeCreate = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
 
-  const [novoDado, setNovoDado] = useState<{id: number, nome : string, imagem : File | null, preco : number}>({
-    id: -1,
-    nome: '',
-    preco: 0,
-    imagem: null,
-  });
-
-  const [idProduto, setIdProduto] = useState<{id : number}>({
-    id : -1,
-  });
-
-  const [resultadosVisiveis, setResultadosVisiveis] = useState(false);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === 'imagem'){
-      if (e.target.files) {
-        setProduto({
-          ...produto,
-          [e.target.name]: e.target.files[0]
-        });
+    setDadosNovoProduto((dadosAnteriores) => {
+      if (name === 'imagem' && files) {
+        const novoDados = new ProdutoParaApi(-1, dadosAnteriores.getNome(), dadosAnteriores.getPreco(), files[0]);
+        return novoDados;
+      } else if (name === 'preco') {
+        const novoDados = new ProdutoParaApi(-1, dadosAnteriores.getNome(), parseFloat(value), dadosAnteriores.getImagem());
+        return novoDados;
+      } else {
+        const novoDados = new ProdutoParaApi(-1, value, dadosAnteriores.getPreco(), dadosAnteriores.getImagem());
+        return novoDados;
       }
-    }else if(e.target.name === 'preco'){
-      setProduto({
-        ...produto,
-        [e.target.name]: parseFloat(e.target.value)
-      });
-    }else{
-      setProduto({
-        ...produto,
-        [e.target.name] : e.target.value
-      });
-    }
+    });
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmitCreate = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData();
-    if(produto.nome === null || produto.nome === '') {
+    const {nome, preco, imagem} = { 
+      nome: dadosMovoProduto.getNome(),
+      preco : dadosMovoProduto.getPreco(),
+      imagem: dadosMovoProduto.getImagem()};
+
+    if(nome === null || nome === '') {
       alert("Adicione um nome");
       return;
     }
-    if(produto.preco === null || produto.preco === 0) {
-      alert("Adicione um preco");
+    if(preco === null || preco <= 0) {
+      alert("Adicione um preco válido.");
       return;
     }
-    if(produto.imagem === null) {
+    if(imagem === null) {
       alert("Adicione uma imagem");
       return;
     }
 
-    formData.append('nome', produto.nome);
-    formData.append('preco', produto.preco.toString());
-    formData.append('imagem', produto.imagem);
+    const formData = new FormData();
+
+    formData.append('nome', nome);
+    formData.append('preco', `${preco}`);
+    formData.append('imagem', imagem);
 
     try {
       const resposta = await fetch(
@@ -83,29 +75,30 @@ const App = () => {
         alert('Erro ao adicionar produto.');
         throw new Error(`HTTP error! status: ${resposta.status}`);
       }
-      
+
       alert('Produto adicionado com sucesso.');
+
+      setResultadosVisiveis({
+        ...resultadosVisiveis,
+        buscaItens : false
+      });
     } catch (error) {
       console.error('Erro:', error);
     }
   }
 
   const handleChangeDelete = (e: ChangeEvent<HTMLInputElement>) => {
-   
-    setIdProduto({
-      ...idProduto,
-      [e.target.name]: e.target.value
-    });
+    setIdDeletarProduto(parseInt(e.target.value));
   }
 
   const handleDelete = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(idProduto.id === null || idProduto.id === -1){
+    if(idDeletarProduto === -1 || Number.isNaN(idDeletarProduto)){
       console.log('Informe o id');
     }
 
     try {
-      const resposta = await fetch(URL + '/api/produtos/' + idProduto.id.toString() + '/', {
+      const resposta = await fetch(URL + '/api/produtos/' + idDeletarProduto.toString() + '/', {
         method: 'DELETE',
       });
 
@@ -115,109 +108,140 @@ const App = () => {
       }
 
       alert('Produto removido com sucesso.');
-      if (idProduto.id === novoDado.id){
-        setNovoDado({
-          id: -1,
-          nome: '',
-          preco: 0,
-          imagem: null,
+      if (idDeletarProduto === novoDadoProduto.getId()){
+        novoDadoProduto.reset();
+        setResultadosVisiveis({
+          buscaItens : false,
+          buscaItem : false
         });
-        setResultadosVisiveis(false);
+      }else{
+        setResultadosVisiveis({
+          ...resultadosVisiveis,
+          buscaItens : false
+        });
       }
     } catch (error) {
       console.error('Erro:', error);
     }
   };
 
-  const handleChangeBusca = (e: ChangeEvent<HTMLInputElement>) => {
-    setIdProduto({
-      ...idProduto,
-      [e.target.name]: e.target.value
-    });
+  const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setIdBuscarProduto(parseInt(e.target.value));
   }
 
-  const handleBusca = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(idProduto.id === -1 || idProduto.id === null){
-      console.log('Informe o ID do produto');
+    if(idBuscarProduto === -1 || Number.isNaN(idBuscarProduto)){
+      alert('Informe o ID do produto');
       return;
     }
 
     try {
-      const resposta = await fetch(URL + '/api/produtos/' + idProduto.id + '/', {
+      const resposta = await fetch(URL + '/api/produtos/' + idBuscarProduto + '/', {
         method: 'GET'
       });
 
       if(!resposta.ok){
-        alert(`Erro ao encontrar o produto de ID ${idProduto}. Verifique se o ID do produto está correto.`);
+        alert(`Erro ao encontrar o produto de ID ${idBuscarProduto}. Verifique se o ID do produto está correto.`);
         throw new Error(`HTTP error! status: ${resposta.status}`);
       }
 
-      setNovoDado({
-        ...novoDado,
-        'id': idProduto.id
-      });
+      setNovoDadoProduto(new ProdutoParaApi(idBuscarProduto,'',0,null));
       const dados = await resposta.json();
       
       if (dados.length > 0) {
-        const produtoEncontrado = dados[0];
-        setProdutoResgatado({
-          nome: produtoEncontrado.nome,
-          preco: produtoEncontrado.preco,
-          imagem: produtoEncontrado.imagem,
-        });
+        const dadosDoProduto = dados[0];
+        setProdutoEncontrado(
+          new ProdutoDaApi(
+            dadosDoProduto.id,
+            dadosDoProduto.nome,
+            dadosDoProduto.preco,
+            dadosDoProduto.imagem)
+        );
 
-        setResultadosVisiveis(true);
+        setResultadosVisiveis({
+          ...resultadosVisiveis,
+          buscaItem : true
+        });
       } else {
-        alert(`Erro ao encontrar o produto de ID ${idProduto.id}. Verifique se o ID do produto está correto.`);
+        alert(`Erro ao encontrar o produto de ID ${idBuscarProduto}. Verifique se o ID do produto está correto.`);
       }
     } catch (error) {
       console.error('Erro:', error);
     }
   };
 
+  const handleProductList = async () => {
+    const resposta = await fetch(URL + '/api/produtos/',{
+      method: 'GET'
+    });
+
+    if(!resposta.ok){
+      alert(`Erro ao encontrar os produtos no Banco de Dados, insira algum produto e tente novamente.`);
+      throw new Error(`HTTP error! status: ${resposta.status}`);
+    }
+
+    const produtosDaApi = await resposta.json();
+
+    const produtosMapeados = produtosDaApi.map((prod: any) => new ProdutoDaApi(prod.id, prod.nome, prod.preco, prod.imagem));
+    setListaProdutos(produtosMapeados);
+    setResultadosVisiveis({
+      ...resultadosVisiveis,
+      buscaItens : true
+    })
+  };
+
   const handleChangeAtualizar = (e : ChangeEvent<HTMLInputElement>) => {
     if (e.target.name === 'imagem'){
-      if (e.target.files) {
-        setNovoDado({
-          ...novoDado,
-          [e.target.name]: e.target.files[0]
+      if (e.target.files !== null) {
+        const file = e.target.files[0];
+        setNovoDadoProduto(() : ProdutoParaApi => {
+          const novosDados = novoDadoProduto
+          novosDados.setImagem(file)
+          return novosDados;
         });
       }
     }else if(e.target.name === 'preco'){
-      setNovoDado({
-        ...novoDado,
-        [e.target.name]: parseFloat(e.target.value)
+      setNovoDadoProduto(() : ProdutoParaApi => {
+        const novosDados = novoDadoProduto
+        novosDados.setPreco(parseFloat(e.target.value));
+        return novosDados;
       });
     }else{
-      setNovoDado({
-        ...novoDado,
-        [e.target.name] : e.target.value
+      setNovoDadoProduto(() : ProdutoParaApi => {
+        const novosDados = novoDadoProduto
+        novosDados.setNome(e.target.value);
+        return novosDados;
       });
     }
   };
 
   const handleAtualiza = async (e : FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const { nome, preco, imagem} = {
+      nome: novoDadoProduto.getNome(),
+      preco: novoDadoProduto.getPreco(),
+      imagem: novoDadoProduto.getImagem()
+    }
     const formData = new FormData();
     let tem_dado = false;
-    if(novoDado.nome !== '' && novoDado.nome !== null){
-      formData.append('nome',novoDado.nome);
+    if(nome !== '' && nome !== null){
+      formData.append('nome', nome);
       tem_dado = true;
     }
-    if(novoDado.preco !== 0 && novoDado.preco !== null){
-      formData.append('preco',novoDado.preco.toString());
+    if(preco !== 0 && preco !== null){
+      formData.append('preco', `${preco}`);
       if(!tem_dado) tem_dado = true;
     }
-    if(novoDado.imagem !== null){
-      formData.append('imagem',novoDado.imagem);
+    if(imagem !== null){
+      formData.append('imagem', imagem);
       if(!tem_dado) tem_dado = true;
     }
 
     if(tem_dado){
       try {
         const resposta = await fetch(
-          URL + '/api/produtos/' + novoDado.id + '/', {
+          URL + '/api/produtos/' + novoDadoProduto.getId() + '/', {
             method: 'PATCH',
             body: formData
           }
@@ -228,14 +252,17 @@ const App = () => {
           throw new Error(`HTTP error! status: ${resposta.status}`);
         }
 
-        setResultadosVisiveis(false);
+        setResultadosVisiveis({
+          buscaItens : false,
+          buscaItem : false
+        });
 
         alert('Produto atualizado com sucesso.');
       } catch (error) {
         console.error('Erro:', error);
       }
     }else{
-      alert(`Preencha ao menos 1 campo para atualizar o produto de ID ${novoDado.id}.`)
+      alert(`Preencha ao menos 1 campo para atualizar o produto de ID ${novoDadoProduto.getId()}.`)
     }
   };
 
@@ -247,7 +274,7 @@ const App = () => {
           <Typography variant="h4" gutterBottom>
             Adicionar Produto
           </Typography>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmitCreate}>
             <Grid container spacing={3}>
               <Grid item xs={12}>
                 <TextField
@@ -255,7 +282,7 @@ const App = () => {
                   fullWidth
                   variant="outlined"
                   name="nome"
-                  onChange={handleChange}
+                  onChange={handleChangeCreate}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -264,14 +291,14 @@ const App = () => {
                   fullWidth
                   variant="outlined"
                   name="preco"
-                  onChange={handleChange}
+                  onChange={handleChangeCreate}
                 />
               </Grid>
               <Grid item xs={12}>
                 <input 
                   type="file" 
                   name="imagem" 
-                  onChange={handleChange} 
+                  onChange={handleChangeCreate} 
                 />
               </Grid>
               <Grid item xs={12} style={{width: '100%'}}><Button variant="contained" color="primary" type='submit'>
@@ -309,7 +336,7 @@ const App = () => {
           <Typography variant="h4" gutterBottom>
             Buscar por Produto
           </Typography>
-          <form onSubmit={handleBusca}>
+          <form onSubmit={handleSearch}>
             <Grid container spacing={3}>
               <Grid item xs={12}>
                 <TextField
@@ -317,7 +344,7 @@ const App = () => {
                   fullWidth
                   variant="outlined"
                   name="id"
-                  onChange={handleChangeBusca}
+                  onChange={handleChangeSearch}
                 />
               </Grid>
               <Grid item><Button variant="contained" color="primary" type='submit'>
@@ -325,18 +352,18 @@ const App = () => {
               </Button></Grid>
             </Grid>
           </form>
-          {resultadosVisiveis && (
+          {resultadosVisiveis.buscaItem && (
             <Grid>
               <Grid container spacing={3} style={{ width: '100%', marginTop: '20px' }}>
                 <Grid item xs={3}>
-                  <img src={URL + produtoResgatado.imagem} alt={produtoResgatado.nome} style={{maxWidth: '100%'}} />
+                  <img src={URL + produtoEncontrado.getImagem()} alt={produtoEncontrado.getNome()} style={{maxWidth: '100%'}} />
                 </Grid>
                 <Grid item xs={9} container direction="column" justifyContent="flex-start">
                   <Grid item style={{marginTop: '20px'}}>
-                    <FormLabel>Nome: {produtoResgatado.nome}.</FormLabel>
+                    <FormLabel>Nome: {produtoEncontrado.getNome()}.</FormLabel>
                   </Grid>
                   <Grid item style={{marginTop: '10px'}}>
-                    <FormLabel>Preço: {produtoResgatado.preco} reais (BR).</FormLabel>
+                    <FormLabel>Preço: {produtoEncontrado.getPreco()} reais (BR).</FormLabel>
                   </Grid>
                 </Grid>
               </Grid>
@@ -381,6 +408,21 @@ const App = () => {
               </Grid>
             </Grid>
           )}
+        </Paper>
+      </Container>
+      <Container style={{'padding': 20}}>
+        <Paper elevation={3} style={{ padding: '20px'}}>
+          <Typography variant="h4">Lista de Produtos</Typography>
+          <List>
+            {resultadosVisiveis.buscaItens && listaProdutos.map((ProdutoDaApi, i) => (
+              <ListItem key={i}>
+                <ListItemText primary={`ID: ${ProdutoDaApi.getId()}`} secondary={`Nome: ${ProdutoDaApi.getNome()}`} />
+              </ListItem>
+            ))}
+          </List>
+          <Button variant="contained" color="primary" onClick={handleProductList}>
+            Carregar Lista
+          </Button>
         </Paper>
       </Container>
     </ThemeProvider>
